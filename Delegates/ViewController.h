9d98ad39e38eaf9690605613d8873f0e6a5d4d19
Javi <javi@Mavericks.local>
2014-10-12 11:03:06 +0200

    Initial Commit

---
+++ Delegates/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Delegates
+//
+//  Created by Javi on 12/10/14.
+//  Copyright (c) 2014 Javi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
